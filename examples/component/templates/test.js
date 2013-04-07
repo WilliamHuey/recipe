48561ebb34318563f7c89cb0876edace07918b82
Lance Pollard <lancejpollard@gmail.com>
2013-04-06 22:10:12 -0700

    readme

--- examples/component/templates/test.js
+++ examples/component/templates/test.js
@@ -4,8 +4,8 @@ var <%= strcase.camelCase(projectName) %> = 'undefined' == typeof window
 
 var assert = require('assert');
 
-describe(<%= strcase.camelCase(projectName) %>, function() {
-  it('should test', function() {
+describe('<%= strcase.camelCase(projectName) %>', function(){
+  it('should test', function(){
     assert.equal(1 + 1, 2);
   });
 });
